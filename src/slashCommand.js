f5dc837a817438d60746f04768df49c3026f0c42
Hope <hopewanroy@gmail.com>
2017-07-01 00:12:20 -0700

    basic responses to slash commands as bot user

---
+++ src/slashCommand.js
@@ -0,0 +1,33 @@
+const slashCommandParser =
+  (slackToken) => body => new Promise((resolve, reject) => {
+    if (!body) {
+
+    }
+
+    if (slackToken !== body.token) {
+
+    }
+
+    console.log(body);
+
+    switch(body.command) {
+      case '/tableflip':
+        return resolve({
+          response_type: 'in_channel',
+          as_user: true,
+          text: '(╯°□°）╯︵ ┻━┻',
+          mrkdwn: false
+        });
+      case '/pleaserespecttables':
+        return resolve({
+          response_type: 'in_channel',
+          as_user: true,
+          text: '┬─┬ノ(@_@ノ)',
+          mrkdwn: false
+        });
+      default:
+        return;
+    }
+  });
+
+module.exports = slashCommandParser;
